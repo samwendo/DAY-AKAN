a413978ee64685c1652b4edc091dfa721bc0961a
samwendo <wendosam21@gmail.com>
2019-08-19 07:46:08 +0300

    add new elements

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var century,year,month,day
+Get input 
+function getinput(){
+    century parseInt(document.getElementById("century").value);
+    year parseInt(document.getElementById("year").value);
+    month parseInt(document.getElementById("month").value);
+    day parseInt(document.getElementById("day").value);
+}
+if (century === ""){
+alert("input the correct centuary");
+return false;
+} else if (year === ""){
+alert("input the correct gender");
+return false;
+} else if (month === "")
+
